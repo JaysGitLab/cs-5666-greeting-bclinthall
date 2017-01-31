634c1faf06856901986603b17dbf5b63ee91dd00
Clint Hall <hallbc@appstate.edu>
2017-01-31 09:39:51 -0500

    write Greeter, GreeterTest, and makefile. So far unable to pass test.

---
+++ Greeter.java
@@ -0,0 +1,8 @@
+public class Greeter{
+    public static void main(String[] args){
+        new Greeter().greet();
+    }
+    public void greet(){
+        System.out.println("Hello, World!");
+    }
+}
---
+++ GreeterTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/*
+ * thanks to dfa at  http://stackoverflow.com/a/1119559
+ */
+public class GreeterTest{
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStream(){
+        System.setOut(new PrintStream(outContent));
+    }
+    @After
+    public void cleanUpStream(){
+        System.setOut(null);
+    }
+
+    @Test
+    public void outputsHello(){
+        Greeter greeter = new Greeter();
+        greeter.greet();
+        assertEquals("failure, should be equal to \"Hello, World!\"", "Hello, World!", outContent.toString());
+    }
+}
