879006282eda268fd82c489ff2c61e51a2e7a56b
Clint Hall <hallbc@appstate.edu>
2017-02-01 16:58:43 -0500

    #6 implemented

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,6 @@
 public class Greeter{
     private String userName="Clint";
+    private String progName="Greeter";
     public static void main(String... args){
         Greeter greeter = new Greeter();
         greeter.greet();
@@ -8,16 +9,31 @@ public class Greeter{
         return userName;
     }
     String getProgName(){
-        return null;
+        return progName;
     }
     public void greet(){
         System.out.println("Hello, "+userName);
     }
     void parseArgs(String... args ){
         int len = args.length;
-        if( len > 0 ){
-            userName = correctCase(args[len-1]);
+        for(int i=0; i<len; i++){
+            String arg = args[i];
+            if(arg.equals("-me")){
+                if(i==len-1){
+                    printUsage();
+                }else{
+                    progName = correctCase(args[i+1]);
+                    i++;
+                }
+            }else if(i==len-1){
+                userName = correctCase(arg);
+            }
         }
+    }
+    void printUsage(){
+        System.out.println("usage: java Greeter [arguments] [user-name]\t\tGreets you by the name you specify, or the default name, 'Clint' if none is specified.");
+        System.out.println("arguments:");
+        System.out.println("\t-me <name>\t\tthe '-me' argument specifies the name which I shall call myself");
     }
     String correctCase(String str){
        if(str.length()==0) return str;
